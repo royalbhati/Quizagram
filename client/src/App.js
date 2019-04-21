af967796ffbeac2fc0ece0a28665d347dbc01849
Royal Bhati <royal240596@gmail.com>
2019-04-21 11:49:54 +0530

    quiz display

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,31 @@ import Landing from './components/Landing'
 import Form from './components/Form'
 import TEMP from './components/temp'
 import quiz from './data/data.json'
+import Question from './components/Question';
+import QuestionCount from './components/QuestionCount';
+import AnswerOption from './components/AnswerOption'
 class App extends Component {
   state={
-    quiz:quiz
+    quiz:quiz,
+    current:0,
+    prev:-1,
+    len:quiz.length
   }
 
-  renderQues =()=>{
-    return this.state.quiz.map((elem)=>{
-      return(
-        <Form quiz={elem}></Form>
-      )
-        }
-    )
+  onClick=()=>{
+    const options = Array.from(document.getElementsByClassName("opts"))
+    options.forEach(elem=>{
+      elem.classList.remove('btn-info')
+      elem.classList.add('btn-outline-info')
+})
+    this.setState(prevState=>({
+      current:prevState.current+1,
+      prev:prevState.current
+    }))
+
+  }
+  renderQues =(i)=>{
+    return (<Form onClick={this.onClick} classBtn ='btn-outline-info' quiz={this.state.quiz[i]}></Form>)
   }
 
   render() {
@@ -25,9 +38,11 @@ class App extends Component {
         <HomeNav></HomeNav>
         {/* <Landing></Landing> */}
         {/* // <Form></Form> */}
-        {this.renderQues()}
-        {/* <TEMP></TEMP> */}
+        {this.state.current>this.state.prev && this.state.len>this.state.current ? this.renderQues(this.state.current):null}
 
+
+        {/* <TEMP></TEMP> */}
+        {/* <Question content="What is your favourite food?" /> */}
       </div>
     );
   }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,39 +2,68 @@ import React, { Component } from 'react'
 import quiz from '../data/data.json'
 export default class Form extends Component {
     state ={
-        // quiz:quiz,
-        answers:[]
+        answers:[
+            {
+                q:0,
+                a:""
+            }
+        ],
+        current:0
     }
     onClick = (event) =>{
-        console.log(this.state.quiz)
+        const options = Array.from(document.getElementsByClassName("opts"))
+        console.log("options",options);
+
 
         if(event.target.classList.contains('btn-outline-info')){
             event.target.classList.remove('btn-outline-info');
+
             event.target.classList.add('btn-info');
+            options.forEach(elem=>{
+                if(elem!==event.target){
+                    elem.classList.remove('btn-info');
+                }
+
+            })
+
         }else{
             event.target.classList.add('btn-outline-info');
             event.target.classList.remove('btn-info');
         }
-    //    this.state.answers.push(event.target.value)
+
     const answer={
         q:this.props.quiz.I,
         a: event.target.value
 
     }
-    this.setState(prevState=>({
-        answers:[...prevState.answers,answer]
-    }))
+    this.setState(prevState=>
+        {
+        const prevOpt=prevState.answers[prevState.answers.length-1].q
+        if(prevState.answers[prevState.answers.length-1].q===answer.q){
+            return {answers:[{q:prevOpt,a:answer.a}]}
+        }else{
+            return {answers:[...prevState.answers,answer]}
+        }
+
+    })
 
     }
 
     onSubmit= (event)=>{
         event.preventDefault();
         console.log(this.state)
-
-
     }
+
+    optionRender=()=>{
+        return this.props.quiz.O.map((elem,i)=>{
+            return(
+                <button onClick ={this.onClick}  value={elem} class=" opts btn btn-outline-info" style={{cursor:'pointer'}}>{elem}</button>
+            )
+        })
+    }
+
   render() {
-    console.log(this.props)
+    console.log("props",this.props)
     return (
       <div className="container mt-5">
           <form onSubmit={this.onSubmit}>
@@ -42,13 +71,13 @@ export default class Form extends Component {
             <label className="ques">Question 1 out of 20</label>
             <h5  class="label" for="exampleInputEmail1">{this.props.quiz.Q}</h5>
         <div className="options">
-        <button onClick ={this.onClick} className="a" value="For UI designing" class="btn btn-outline-info" style={{cursor:'pointer'}}>For UI designing</button>
-        <button onClick ={this.onClick} className="b"  value="For backend connection" class="btn btn-outline-info" style={{cursor:'pointer'}}>For backend connection</button>
-        <button onClick ={this.onClick} className="c" value="For cleaning toilet" class="btn btn-outline-info" style={{cursor:'pointer'}}>For cleaning toilet</button>
-        <button onClick ={this.onClick} className="d"  value="For nothing" class="btn btn-outline-info" style={{cursor:'pointer'}}>For nothing</button>
+
+            {this.optionRender()}
 
         </div>
-        <input type="submit" ></input>
+        <div className="next">
+        <button className="btn btn-primary mt-5 btn-lg" onClick={this.props.onClick}>Next</button>
+        </div>
         </div>
           </form>
       </div>
